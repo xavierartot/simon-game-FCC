103499eebc9546ddb09c67796aea7090c92d4667
xavierartot <xavierartot@gmail.com>
2016-12-14 22:00:44 -0600

    simon game done

--- src/babel/script.js
+++ src/babel/script.js
@@ -28,6 +28,7 @@ let Simon = ( () => {
   random      = '',
   as = data.arraySimon,
   ao = data.arrayOppoment,
+  clickCount, 
   randomTemp  = ''
 
   // private
@@ -50,7 +51,6 @@ let Simon = ( () => {
 
   let addOneStep = () => {
     let ide = 0, randNumber 
-    //showShape()
     if (ide === 0) {
       ide++
       randNumber = randAddItem()
@@ -58,6 +58,7 @@ let Simon = ( () => {
       //console.log('one step ' + as, ao);
       setTimeout(function () {
         //add sound with randomTemp 
+        showShape()
         for (let prop in data){
           if (data.hasOwnProperty(prop)) {
             let d =data[prop] 
@@ -68,30 +69,108 @@ let Simon = ( () => {
               d.classList.add('active')
               _playSound(dataGame[randNumber].sound)
               data.countBox.textContent = as.length 
-              console.log('i addOneStep ');
+              //console.log('i addOneStep ');
             }
           }
         }
-      }, 2000);//1s
+      }, 1500);//1s
 
       setTimeout(function () {
         removeActiveClass()
-        //showShape()
+        showShape()
         emptyDataArray(ao)//delete arrayOppoment
         //console.log(ide);
-      }, 2600);//1s
+      }, 2000);//1s
       //return randNumber;
     }
   }
 
   let _ai = () => {
-    let anim, total=0 //, d = data.arraySimon
+    let anim, total=0, flag //, d = data.arraySimon
     //console.log(as);
     if (as.length === 0) {
       //add a step if arraySimon it's === 0
       addOneStep()
-    } else if(as.length > 0){
-      //console.log(as);
+    } else if(as.length > 0 && as.length < 20){
+      for (let i = 0, l = as.length; i < l; i++) {
+        let current = as[i], prev = as[i-1];
+        if (as.length === ao.length) {
+          if (as[i] === ao[i]) {
+            //console.log('succes game');
+            flag = true
+          } else{
+            //console.log('les tab sont diff');
+            errorGame(2000)
+            flag = false
+          } 
+        } else {
+          //console.log('les tab longueur diff');
+          errorGame(2000)
+          flag = false
+        }
+      }
+      if (flag === true) {
+        hideShape()
+        //console.log(as);
+        as.forEach(function (cle, i, origin) {
+          anim = setTimeout(function(i) { // i is replace by j   
+            for (let prop in data){
+              if (data.hasOwnProperty(prop)) {
+                if (prop === dataGame[cle].color) {
+                  setTimeout(function () {
+                    $('.'+prop+', .shape__color-'+prop).addClass('active')
+                    _playSound(dataGame[cle].sound)
+                  }, 2000);//1s
+                  setTimeout(function () {
+                    //hideShape()
+                    removeActiveClass()
+                    //showShape()
+                  }, 2500);//1s
+                }
+              }
+            } //end forin
+          }, i * 1000, i); // we're passing i in the argument of the callBack
+          total = i * 1000
+        });// end foreach
+
+        for (let i = 0, l = as.length; i < l; i++) {
+          //console.log(i);
+          if ( (as.length-1) === i) {
+            return setTimeout(function () {
+              addOneStep()
+            }, total + 2000);//1s
+            break;
+          }
+        }
+
+      }
+    } else if(as.length >= 20){
+       alert("you're win")
+      emptyDataArray(as)// delete arraySimon
+        //start circle - change the circle a rouge
+        data.startActive.classList.remove('active')
+        //remove class error
+        data.countBox.classList.remove('error')
+        data.countBox.textContent = ''
+        //remove class active
+        removeActiveClass()
+        clickCount = 0//used in click
+        return false
+    } 
+    // end else if(as.length > 0)
+  }// end _ai()
+
+  let errorGame = (time) => {
+    let anim, total=0//, d = data.arraySimon
+    clickCount = 0//used in click
+    _playSound(errorSound )
+    
+    data.countBox.textContent = 'err'//data.countbox === text box
+    data.countBox.classList.add('error')
+    emptyDataArray(ao)//delete arrayOppoment
+
+    return setTimeout(function () {
+      hideShape()
       as.forEach(function (cle, i, origin) {
         //console.log(`cle = ${cle}, val= ${as[cle]}, index = ${i}, origin = ${i}`);
         anim = setTimeout(function(i) { // i is replace by j   
@@ -99,13 +178,15 @@ let Simon = ( () => {
             if (data.hasOwnProperty(prop)) {
               if (prop === dataGame[cle].color) {
                 setTimeout(function () {
-                  //showShape()
-                  $('.'+prop).addClass('active')
+                  //$('.'+prop).addClass('active')
+                  $('.'+prop+', .shape__color-'+prop).addClass('active')
+                  //$(this).after( '<div class="shape__color-blue temp">i</div>' )
                   _playSound(dataGame[cle].sound)
                 }, 2000);//1s
                 setTimeout(function () {
                   //hideShape()
                   removeActiveClass()
+                  //showShape()
                 }, 2500);//1s
               }
             }
@@ -113,31 +194,11 @@ let Simon = ( () => {
         }, i * 1000, i); // we're passing i in the argument of the callBack
         total = i * 1000
       });// end foreach
-
-      console.log(as);
-      console.log(total);
-      for (let i = 0, l = as.length; i < l; i++) {
-        console.log(i);
-        if ( (as.length-1) === i) {
-          //alert(as.length-1)
-          //break;
-          return setTimeout(function () {
-            addOneStep()
-          }, total + 2000);//1s
-          break;
-        }
-      }
-    }// end else if(as.length > 0)
-  }// end _ai()
-
-  let error = (time) => {
-    return setTimeout(function () {
-      _playSound(errorSound )
-      //data.countbox === text box
-      data.countBox.textContent = 'err'
-      data.countBox.classList.add('error')
-      //send to different ai() ex: _aiError()
-      _ai()
+      showShape()
+      removeActiveClass()
+      data.countBox.textContent = as.length
+      data.countBox.classList.remove('error')
+      
     }, time);//1s
   }
 
@@ -164,6 +225,7 @@ let Simon = ( () => {
         data.countBox.textContent = ''
         //remove class active
         removeActiveClass()
+        clickCount = 0//used in click
         return false
       } else{
         return true
@@ -213,47 +275,53 @@ let Simon = ( () => {
   }
 
   let clickShape = () => {
-    let clickCount = 0, numClick = 0 
+    let numClick = 0 , num, sound
+    clickCount = 0
     $('.shape__color').on('click', function (e) {
-      //hold the number clicked
-      numClick = parseInt(this.textContent.trim(),10)
-      //console.log(numClick);
-      randAddItem(numClick)//push the click in ao (array oppoment)
+      console.log( as);
+      console.log( ao);
+      numClick = parseInt(this.textContent.trim(),10)//hold the number clicked
+      num = randAddItem(numClick)//push the click in ao (array oppoment)
       clickCount++ //count the click number
-      let sound = dataGame[parseInt(this.textContent.trim(),10)].sound
-      //console.log('click ' + as, ao);
+      sound = dataGame[parseInt(this.textContent.trim(),10)].sound//sound
+      console.log(as.length);
+      console.log(clickCount);
       if (as.length === 1 && clickCount === 1) {
-        //console.log('click ' + as, ao);
         _playSound(sound)
         this.classList.add('activeClick')
         setTimeout(function () {
           removeActiveClass()
           //console.log('ai click 1');
-          _ai()
+          return _ai()
         }, 800);//1s
-        return this
       } else if(as.length > 1){ 
         //console.log('click ' + as, ao);
-        for (let i = 0, l = as.length; i < l; i++) {
+        loop: for (let i = 0, l = as.length; i < l; i++) {
+          //console.log(as, ao);
+          //console.log(as[i], ao[i]);
           if (as[i]=== ao[i])  {
+            //console.log('equal');
             _playSound(sound)
             this.classList.add('activeClick')
             setTimeout(function () {
               removeActiveClass()
             }, 800);//1s
             //console.log('1 valeur dans ao');
-            if(as.length === ao.length){
+            if(as.length === ao.length){//same size the result is complete
               setTimeout(function () {
                 //console.log('click same size' + as, ao);
                 removeActiveClass()
                 return _ai()
               }, 800);//1s
-              break;
+              break loop;
             }
+            //continue loop
+            break loop
           } else if(as[i] !== ao[i]){
-            //console.log('nope succes');
+            console.log('nope equal');
+            break;
           } else{
-            //console.log('error');
+            console.log('error');
             //return error()
             //return _ai()
           } 
@@ -265,19 +333,31 @@ let Simon = ( () => {
   }
 
   let removeActiveClass = () => {
-    for (let i = 0, l = data.shape.length; i < l; i++) {
-      let current = data.shape[i]
-      if (current.classList.contains('active') === true) {
-        current.classList.remove('active')
-      } else if(current.classList.contains('activeClick') === true){
-        current.classList.remove('activeClick')
+    $('.temp, .shape__color').removeClass('active activeClick')
+    //$('.temp').removeClass('activeClick')
+    //for (let i = 0, l = data.shape.length; i < l; i++) {
+    //let current = data.shape[i]
+    //if (current.classList.contains('active') === true ) {
+    //current.classList.remove('active')
+    //} else if(current.classList.contains('activeClick') === true){
+    //current.classList.remove('activeClick')
 
-      } 
-    }
+    //} 
+    //}
   }
 
   //main
   let main  = () => {
+    //let arr1 = [2,5,7,19]
+    //let arr2 = [2,5,9,19]
+    //for (let i = 0, l = arr1.length; i < l; i++) {
+    //let current = arr1[i], prev = arr1[i-1];
+    //if (arr1[i] === arr2[i]) {
+    //console.log('a, ' +arr1[i]);     
+    //} else{
+    //console.log('b, ' +arr2[i]);
+    //} 
+    //}
     clickShape()
     _start()
     removeActiveClass()
